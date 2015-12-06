8c8f9c698ca32df6cad73e2e0cf46e648091f16d
Adland Lee <adland@4mfd.com>
2015-12-05 22:58:04 -0800

    add initial notes

---
+++ fundamentals.py
@@ -0,0 +1,17 @@
+# fundamentals.py
+
+print "Hello World."
+
+title = "Fundamentals"
+
+print "Examples of %s" % title
+
+name = raw_input("Your name: ")
+
+print "Greetings, %s" % name
+
+def defaultGreeting():
+    print "A default greeting"
+
+defaultGreeting()
+
