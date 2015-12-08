6c5d66c9ed76715ae93f67452402173c71183845
Adland Lee <adland@4mfd.com>
2015-12-08 11:56:56 -0800

    add console I/O review exercise

--- input-output.py
+++ input-output.py
@@ -3,3 +3,14 @@
 # review console I/O
 # CLI parameters
 # file I/O
+
+# input from keyboard
+def getUserName():
+    aName = raw_input("Your name: ")
+    return aName
+
+def printGreeting(aName):
+    print "Greetings, %s" % aName
+
+userName = getUserName()
+printGreeting(userName)
